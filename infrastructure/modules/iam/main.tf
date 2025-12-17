fcd8d759bde933027fd10772b265de6ee77610d5
Nurellezia Suleiman <ellezia.suleiman@yahoo.com>
2025-12-17 10:41:15 +0800

    feat(infra): create scoped iam role with dynamodb and cloudwatch permissions

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -10,3 +10,29 @@ resource "aws_iam_role" "this" {
   })
   tags = var.tags
 }
+
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "dynamodb_access" {
+  name = "${var.service_name}-dynamodb-policy"
+  role = aws_iam_role.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:PutItem",
+        "dynamodb:GetItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan"
+      ]
+      Resource = var.dynamodb_table_arn
+    }]
+  })
+}
--- infrastructure/modules/iam/variables.tf
+++ infrastructure/modules/iam/variables.tf
@@ -1,11 +1,20 @@
 variable "service_name" {
   type = string
+  description = "The name of the service."
 }
+
 variable "service_principal" {
   type    = string
   default = "lambda.amazonaws.com"
+  description = "The AWS service allowed for this role"
 }
+
 variable "tags" {
   type    = map(string)
   default = {}
+}
+
+variable "dynamodb_table_arn" {
+  type        = string
+  description = "The ARN of the DynamoDB table this role should have access to."
 }
